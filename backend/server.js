677bd2338ef331760eaa6185c4310d101e86501b
sapphirebroking <nakul.thakur@sapphirebroking.com>
2026-04-18 22:18:22 +0530

    fix: db func related issues

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
 import app from "./src/app.js";
+import connectDB from './src/config/db.js';
+
+connectDB();
 
 app.listen(8080, (req, res) => {
     console.log('Server is up and running at port', 8080);
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 import env from "./env.js";
 
-export async function ConnectDB() {
+async function ConnectDB() {
   await mongoose.connect(env.MONGO_URI);
   console.log("Databse connected successfully");
 }
+
+export default ConnectDB;
